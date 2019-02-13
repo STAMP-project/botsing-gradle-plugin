8a8cb9c884e3537aa4d7d89e2ddb74c0814155a1
MaelAudren <mael.audren@activeeon.com>
2019-02-13 16:10:11 +0100

    upgrade botsing version to 1.0.3

--- src/main/java/eu/stamp_project/botsing/BotsingGradlePluginExtension.java
+++ src/main/java/eu/stamp_project/botsing/BotsingGradlePluginExtension.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.commons.io.FilenameUtils;
-
 import eu.stamp.botsing.Botsing;
 import lombok.Getter;
 import lombok.Setter;
@@ -60,7 +58,7 @@ public class BotsingGradlePluginExtension {
         //required parameters
         addRequiredParameter("target_frame", targetFrame);
         addRequiredParameter("crash_log",logPath);
-        addRequiredParameter("projectCP", libsPath);
+        addRequiredParameter("project_cp", libsPath);
 
         //optional parameters
         Optional.ofNullable(output).ifPresent(value -> commands.add(String.format("-Dtest_dir=%s",Paths.get(output).toString())));
@@ -80,36 +78,6 @@ public class BotsingGradlePluginExtension {
         commands.add(String.format("-%s",parameter));
         commands.add(value);
     }
-
-    private void addProperty(String commandName, String property){
-        commands.add(String.format("-D%s=%s",commandName,property));
-    }
-
-    private void addOptionalProperty(String commandName, String property){
-        Optional.ofNullable(property).filter(command -> ! command.isEmpty()).ifPresent(command -> addProperty(commandName,command));
-    }
-
-    /**
-     * Concatenate all the dependencies of the softwareLibsPath
-     * @return
-     */
-    private String getDependencies(String libPath){
-        String dep = "";
-        File lib_dir = new File(libPath);
-        File[] listOfFilesInSourceFolder = lib_dir.listFiles();
-        for(int i = 0; i < listOfFilesInSourceFolder.length; i++){
-            String lib_file_name = listOfFilesInSourceFolder[i].getName();
-            // Do not consider non jar files
-            if( listOfFilesInSourceFolder[i].getName().charAt(0) !='.' && FilenameUtils.getExtension(lib_file_name).equals("jar")) {
-                Path depPath = Paths.get(lib_dir.getAbsolutePath(), lib_file_name);
-                String dependency = depPath.toString();
-                dep += (dependency+":");
-            }
-
-        }
-        return dep.substring(0, dep.length() - 1);
-    }
-
 
     private void checkIfDirectoryExists(String directoryPath){
         if(! new File(directoryPath).exists()){

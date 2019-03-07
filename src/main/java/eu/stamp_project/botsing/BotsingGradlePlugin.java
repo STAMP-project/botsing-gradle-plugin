fcaa55e86be85549944874b6e4800db50cc23a66
MaelAudren <mael.audren@activeeon.com>
2019-03-07 18:55:52 +0100

    reformat code and automatic dependency fetching

--- src/main/java/eu/stamp_project/botsing/BotsingGradlePlugin.java
+++ src/main/java/eu/stamp_project/botsing/BotsingGradlePlugin.java
@@ -3,20 +3,24 @@ package eu.stamp_project.botsing;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
+
 public class BotsingGradlePlugin implements Plugin<Project> {
 
-	@Override
-	public void apply(Project project) {
+    @Override
+    public void apply(Project project) {
 
-	    BotsingGradlePluginExtension extension = project.getExtensions().create("botsing", BotsingGradlePluginExtension.class);
+        BotsingGradlePluginExtension extension = project.getExtensions().create("botsing",
+                BotsingGradlePluginExtension.class);
 
-	    project.task("botsing").doLast(task -> {
-	        extension.checkProperties();
-			try {
-				 extension.create();
-			}catch (Exception e){
-				System.err.println("An exception occured during the generation: \n"+e.getMessage());
-			}
-	    });
-	}
+        project.task("botsing").doLast(task -> {
+            extension.checkProperties();
+
+            try {
+                extension.create(project);
+            } catch (Exception e) {
+                System.err.println("An exception occured during the generation: \n" + e.getMessage());
+            }
+        });
+    }
+
 }
--- src/main/java/eu/stamp_project/botsing/BotsingGradlePluginExtension.java
+++ src/main/java/eu/stamp_project/botsing/BotsingGradlePluginExtension.java
@@ -1,96 +1,122 @@
 package eu.stamp_project.botsing;
 
 import java.io.File;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.gradle.api.Project;
 
 import eu.stamp.botsing.Botsing;
 import lombok.Getter;
 import lombok.Setter;
 
+
 public class BotsingGradlePluginExtension {
 
     //Required parameters
-    @Getter @Setter
+    @Getter
+    @Setter
     private String logPath = null;
-    @Getter @Setter
-    private String libsPath =null;
-    @Getter @Setter
+
+    @Getter
+    @Setter
     private String targetFrame = null;
 
     //Optional parameters
-    @Getter @Setter
-    private String output= null;
-    @Getter @Setter
-    private String searchBudget= null;
-    @Getter @Setter
-    private String population= null;
+    @Getter
+    @Setter
+    private String output = null;
 
+    @Getter
+    @Setter
+    private String searchBudget = null;
+
+    @Getter
+    @Setter
+    private String population = null;
 
     private List<String> commands;
 
-    public BotsingGradlePluginExtension(){
+    public BotsingGradlePluginExtension() {
         commands = new ArrayList<>();
     }
 
-    public void checkProperties(){
+    public void checkProperties() {
 
         System.out.println("The following parameters will be used: ");
-        checkRequiredParameterContent("logPath",logPath);
+        checkRequiredParameterContent("logPath", logPath);
         checkIfDirectoryExists(logPath);
-        checkRequiredParameterContent("libsPath",libsPath);
-        checkIfDirectoryExists(libsPath);
-        checkRequiredParameterContent("targetFrame",targetFrame);
+        checkRequiredParameterContent("targetFrame", targetFrame);
 
-        displayOptionalParameterIfNotNull("output",output);
+        displayOptionalParameterIfNotNull("output", output);
         Optional.ofNullable(output).ifPresent(path -> checkIfDirectoryExists(path));
-        displayOptionalParameterIfNotNull("searchBudget",searchBudget);
-        displayOptionalParameterIfNotNull("population",population);
+        displayOptionalParameterIfNotNull("searchBudget", searchBudget);
+        displayOptionalParameterIfNotNull("population", population);
     }
 
-    public void create() {
+    public void create(Project project) {
 
         String logPath = this.logPath;
 
         //required parameters
         addRequiredParameter("target_frame", targetFrame);
-        addRequiredParameter("crash_log",logPath);
-        addRequiredParameter("project_cp", libsPath);
+        addRequiredParameter("crash_log", logPath);
+        addRequiredParameter("project_cp", getDependencies(project));
 
         //optional parameters
-        Optional.ofNullable(output).ifPresent(value -> commands.add(String.format("-Dtest_dir=%s",Paths.get(output).toString())));
-        Optional.ofNullable(searchBudget).ifPresent(val -> commands.add(String.format("-Dsearch_budget=%s",searchBudget)));
-        Optional.ofNullable(population).ifPresent(val -> commands.add(String.format("-Dpopulation=%s",population)));
+        Optional.ofNullable(output)
+                .ifPresent(value -> commands.add(String.format("-Dtest_dir=%s", Paths.get(output).toString())));
+        Optional.ofNullable(searchBudget)
+                .ifPresent(val -> commands.add(String.format("-Dsearch_budget=%s", searchBudget)));
+        Optional.ofNullable(population).ifPresent(val -> commands.add(String.format("-Dpopulation=%s", population)));
 
-        System.out.println(commands);
+        System.out.println("commands : " + commands);
 
         try {
-            new Botsing().parseCommandLine(commands.stream().toArray(String[]::new));
-        }catch (Exception e){
-            System.out.println("An error happened while generating a new output: "+e.getMessage());
+            new Botsing().parseCommandLine(commands.toArray(new String[0]));
+        } catch (Throwable e) {
+            System.out.println("An error happened while running Botsing: " + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 
-    private void addRequiredParameter(String parameter, String value){
-        commands.add(String.format("-%s",parameter));
+    private void addRequiredParameter(String parameter, String value) {
+        commands.add(String.format("-%s", parameter));
         commands.add(value);
     }
 
-    private void checkIfDirectoryExists(String directoryPath){
-        if(! new File(directoryPath).exists()){
-            throw new RuntimeException(String.format("Bad path %s",directoryPath));
+    private void checkIfDirectoryExists(String directoryPath) {
+        if (!new File(directoryPath).exists()) {
+            throw new RuntimeException(String.format("Bad path %s", directoryPath));
         }
     }
 
-    private void checkRequiredParameterContent(String parameterName, String parameter){
-        System.out.println(String.format(" - %s: %s",parameterName, Optional.ofNullable(parameter)
-                .orElseThrow(() -> new RuntimeException(String.format("Impossible to run Botsing, %s is not set",parameterName)))));
+    private void checkRequiredParameterContent(String parameterName, String parameter) {
+        System.out.println(String.format(" - %s: %s",
+                parameterName,
+                Optional.ofNullable(parameter)
+                        .orElseThrow(() -> new RuntimeException(String.format("Impossible to run Botsing, %s is not set",
+                                parameterName)))));
     }
 
-    private void displayOptionalParameterIfNotNull(String parameterName, String parameter){
-        Optional.ofNullable(parameter).ifPresent(content -> System.out.println(String.format("%s: %s",parameterName,content)));
+    private void displayOptionalParameterIfNotNull(String parameterName, String parameter) {
+        Optional.ofNullable(parameter)
+                .ifPresent(content -> System.out.println(String.format("%s: %s", parameterName, content)));
+    }
+
+    private String getDependencies(Project project) {
+
+        return project.getConfigurations()
+                .stream()
+                .map(configuration -> configuration.resolve()
+                        .stream()
+                        .map(File::getAbsolutePath)
+                        .filter(file -> file.endsWith(".jar"))
+                        .collect(Collectors.joining(File.pathSeparator)))
+                .collect(Collectors.joining(File.pathSeparator));
+
     }
 }

0ac39cc40700861fa86639d3e9b8a704ffad8b42
MaelAudren <mael.audren@activeeon.com>
2019-04-19 15:43:38 +0200

    get artifacts from maven instead of using local dependencies

--- src/main/java/eu/stamp_project/botsing/BotsingGradlePluginExtension.java
+++ src/main/java/eu/stamp_project/botsing/BotsingGradlePluginExtension.java
@@ -3,8 +3,10 @@ package eu.stamp_project.botsing;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.gradle.api.Project;
@@ -17,8 +19,6 @@ import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class BotsingGradlePluginExtension {
-
-    private final String botsingConfigName = "botsingConfig";
 
     //Required parameters
     @Getter
@@ -46,24 +46,31 @@ public class BotsingGradlePluginExtension {
     @Setter
     private String population = null;
 
+    @Getter
+    @Setter
+    private List<String> artifacts = null;
+
     private List<String> commands;
 
     public BotsingGradlePluginExtension() {
         commands = new ArrayList<>();
     }
 
+    /**
+     * Ensure that the user parameters are correct
+     */
     public void checkProperties() {
-
         log.info("The following parameters will be used: ");
         checkRequiredParameterContent("logPath", logPath);
         checkIfDirectoryExists(logPath);
         checkRequiredParameterContent("targetFrame", targetFrame);
 
         displayOptionalParameterIfNotNull("output", output);
-        Optional.ofNullable(output).ifPresent(path -> checkIfDirectoryExists(path));
+        Optional.ofNullable(output).ifPresent(this::checkIfDirectoryExists);
         displayOptionalParameterIfNotNull("searchBudget", searchBudget);
         displayOptionalParameterIfNotNull("population", population);
         displayOptionalParameterIfNotNull("botsingVersion", botsingVersion);
+        displayOptionalListParameterIfNotNull("artifacts",artifacts);
     }
 
     public void create(Project project) {
@@ -81,7 +88,7 @@ public class BotsingGradlePluginExtension {
         Optional.ofNullable(population).ifPresent(val -> commands.add(String.format("-Dpopulation=%s", population)));
 
         if (log.isDebugEnabled()){
-            commands.stream().peek(command -> log.debug(String.format("Command : %s",command)));
+            commands.forEach(command -> log.debug(String.format("Command : %s",command)));
         }
 
         try {
@@ -116,35 +123,48 @@ public class BotsingGradlePluginExtension {
                 .ifPresent(content -> displayParameter(parameterName,content));
     }
 
+    private void displayOptionalListParameterIfNotNull(String parameterName, List<String> listParameter){
+        Optional.ofNullable(listParameter)
+                .ifPresent(content -> {
+                    String formattedList = content.stream().map(artifact -> String.format("\n %s",artifact)).collect(Collectors.joining(","));
+                    log.info(String.format("- %s: [%s]", parameterName, formattedList));
+                });
+    }
+
     private void displayParameter(String parameterName, String parameter){
         log.info(String.format("- %s: %s", parameterName, parameter));
     }
 
     private String getDependencies(Project project) {
+        final String projectMavenName = "projectMavenConfig";
+        Configuration projectMavenConfig = project.getConfigurations().create(projectMavenName);
 
-        return project.getConfigurations()
-                .stream()
-                .filter(configuration -> ! configuration.getName().equals(botsingConfigName))
-                .map(configuration -> configuration.resolve()
-                        .stream()
-                        .map(File::getAbsolutePath)
-                        .filter(file -> file.endsWith(".jar"))
-                        .collect(Collectors.joining(File.pathSeparator)))
-                .collect(Collectors.joining(File.pathSeparator))
-                .substring(1);
+        // Take the artifact provided by the user otherwise use the current version of the project and download it from maven
+        List<String> projectMavenRef = Optional.ofNullable(artifacts)
+                .orElse(Collections.singletonList(String.format("%s:%s:%s",project.getGroup(),project.getName(),project.getVersion())));
 
+        projectMavenRef.forEach(ref -> projectMavenConfig.getDependencies()
+                .add(project.getDependencies().create(ref)));
+
+        //Resolve the configuration and download the files
+        Set<File> jarFiles = projectMavenConfig.resolve();
+
+        return jarFiles.stream()
+                .map(File::getAbsolutePath)
+                .filter(file -> file.endsWith(".jar"))
+                .peek(file -> log.info("file "+file))
+                .collect(Collectors.joining(File.pathSeparator));
     }
 
+    // Download botsing and add it as a dependency to the current artifact
     private File addBotsingDependencies(Project project){
+        final String botsingConfigName = "botsingConfig";
         final String defaultBotsingVersion = "1.0.4";
         final String mavenRef = "eu.stamp-project:botsing-reproduction:" + Optional.ofNullable(botsingVersion)
                 .orElse(defaultBotsingVersion);
 
         Configuration botsingConfig = project.getConfigurations().create(botsingConfigName);
         botsingConfig.getDependencies().add(project.getDependencies().create(mavenRef));
-        return project.getConfigurations().getByName(botsingConfigName).resolve()
-                .stream()
-                .collect(Collectors.toList())
-                .get(0);
+        return new ArrayList<>(project.getConfigurations().getByName(botsingConfigName).resolve()).get(0);
     }
 }
